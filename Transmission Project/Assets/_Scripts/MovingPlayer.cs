a641be78563bd3853c8f457e43f0387691db02b5
Samuel Schwab <sam2schwab@gmail.com>
2018-01-27 03:58:24 -0500

    ebauche de deplacement entre les tavernes

---
+++ Transmission Project/Assets/_Scripts/MovingPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlayer : MonoBehaviour {
+
+    private const float DURATION = 1;
+
+    private bool moving;
+    private AnimationCurve anim;
+    private Vector3 dest;
+    private Vector3 movement;
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        moving = false;
+        anim = AnimationCurve.EaseInOut(0, 0, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving)
+        {
+            float t = (Time.time - startTime) / DURATION;
+            Vector3 position;
+            if (t < 1.0f)
+            {
+                float ratio = anim.Evaluate(t);
+                position = dest - movement * (1 - ratio);
+            }
+            else
+            {
+                position = dest; //1 or larger means we reached the end
+                moving = false;
+            }
+            transform.position = position;
+        }
+	}
+
+    public void MoveTo(Vector3 destination)
+    {
+        if (!moving)
+        {
+            moving = true;
+            startTime = Time.time;
+            dest = destination;
+            movement = destination - transform.position;
+        }
+    }
+}
---
+++ Transmission Project/Assets/_Scripts/Selectable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour {
+
+    GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnMouseEnter()
+    {
+        GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().flipX = false;
+    }
+
+    private void OnMouseDown()
+    {
+        player.GetComponent<MovingPlayer>().MoveTo(transform.position);
+    }
+}
